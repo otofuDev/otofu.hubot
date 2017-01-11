021fd7c8f67b7200e67ac2a83bcfcecb21e4ef6c
Motoki Kobayashi <otofu.xxx@gmail.com>
2017-01-11 23:54:44 +0900

    水曜どうでしょうおみくじ追加

---
+++ scripts/fortune.js
@@ -0,0 +1,26 @@
+// Description:
+//   水曜どうでしょうおみくじ.
+//
+// Commands:
+//   hubot おみくじ - 水曜どうでしょうのおみくじを引く.
+//
+// Author:
+//   0tofu <otofu.xxx@gmail.com>
+
+const co = require('co');
+const ScreenShot = require('../lib/ScreenShot');
+
+module.exports = (robot) => {
+  robot.respond(/おみくじ/, (msg) => {
+    co(function*(){
+      const gyazo_res = yield ScreenShot.capture('http://www.htb.co.jp/suidou/omikuji.html', 'img');
+      const attachments = {
+        fallback: 'おみくじ',
+        title: 'おみくじ',
+        image_url: gyazo_res.url,
+      };
+      const client = robot.adapter.client;
+      client.web.chat.postMessage(msg.envelope.room, '', {as_user: true, attachments: [attachments]});
+    });
+  });
+};
