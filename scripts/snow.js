feafba77d0b0422cc1c69f56e69ad8c139519fc5
Motoki Kobayashi <otofu.xxx@gmail.com>
2017-02-23 23:57:32 +0900

    コメントアウト誤り

--- scripts/snow.js
+++ scripts/snow.js
@@ -33,31 +33,33 @@ module.exports = (robot) => {
     msg.finish();
   });
 
-#  const cron_execute = /^production$/i.test(process.env.NODE_ENV);
-#  new CronJob('0 0 9 * * *', () => {
-#    co(function*() {
-#      // 奥伊吹スキー場の積雪情報を取得.
-#      const info_res = yield ScreenShot.capture(TAKASU_INFO_URL, '.weather_infoBox');
-#      const info_attachments = {
-#        fallback: '高鷲スノーパーク 積雪情報',
-#        title: '高鷲スノーパーク 積雪情報',
-#        title_link: TAKASU_INFO_URL,
-#        image_url: info_res.url,
-#      };
-#
-#      // 奥伊吹スキー場の気象情報を取得.
-#      const weather_res = yield ScreenShot.capture(TAKASU_WEATHER_URL, '#weather');
-#      const weather_attachments = {
-#        fallback: '高鷲スノーパーク 気象情報',
-#        title: '高鷲スノーパーク 気象情報',
-#        title_link: TAKASU_WEATHER_URL,
-#        image_url: weather_res.url,
-#      };
-#
-#      const client = robot.adapter.client;
-#      const channel_id = robot.adapter.client.rtm.dataStore.getChannelOrGroupByName('-general').name;
-#      client.web.chat.postMessage(channel_id, '', {as_user: true, attachments: [info_attachments]});
-#      client.web.chat.postMessage(channel_id, '', {as_user: true, attachments: [weather_attachments]});
-#    });
-#  }, null, cron_execute);
+/*
+  const cron_execute = /^production$/i.test(process.env.NODE_ENV);
+  new CronJob('0 0 9 * * *', () => {
+    co(function*() {
+      // 奥伊吹スキー場の積雪情報を取得.
+      const info_res = yield ScreenShot.capture(TAKASU_INFO_URL, '.weather_infoBox');
+      const info_attachments = {
+        fallback: '高鷲スノーパーク 積雪情報',
+        title: '高鷲スノーパーク 積雪情報',
+        title_link: TAKASU_INFO_URL,
+        image_url: info_res.url,
+      };
+
+      // 奥伊吹スキー場の気象情報を取得.
+      const weather_res = yield ScreenShot.capture(TAKASU_WEATHER_URL, '#weather');
+      const weather_attachments = {
+        fallback: '高鷲スノーパーク 気象情報',
+        title: '高鷲スノーパーク 気象情報',
+        title_link: TAKASU_WEATHER_URL,
+        image_url: weather_res.url,
+      };
+
+      const client = robot.adapter.client;
+      const channel_id = robot.adapter.client.rtm.dataStore.getChannelOrGroupByName('-general').name;
+      client.web.chat.postMessage(channel_id, '', {as_user: true, attachments: [info_attachments]});
+      client.web.chat.postMessage(channel_id, '', {as_user: true, attachments: [weather_attachments]});
+    });
+  }, null, cron_execute);
+*/
 };
