a2cfbbc1f12b04c5c28daa9be50674f7b1d8c5e6
Motoki Kobayashi <motoki.kobayashi@studio-umi.jp>
2017-04-05 14:22:00 +0900

    複数URLに対応

--- scripts/watch_switch.js
+++ scripts/watch_switch.js
@@ -3,6 +3,14 @@ const CronJob = require('cron').CronJob;
 const client = require('cheerio-httpcli');
 const Utils = require('../lib/Utils');
 
+const watch_lists = {
+  yodobashi: {
+    name: 'ヨドバシカメラ',
+    url: 'http://www.yodobashi.com/%E4%BB%BB%E5%A4%A9%E5%A0%82-Nintendo-Nintendo-Switch-Joy-Con-L-%E3%83%8D%E3%82%AA%E3%83%B3%E3%83%96%E3%83%AB%E3%83%BC-R-%E3%83%8D%E3%82%AA%E3%83%B3%E3%83%AC%E3%83%83%E3%83%89-Nintendo-Switch%E6%9C%AC%E4%BD%93/pd/100000001003431566/',
+    elm: '.buyBoxMain .salesInfo',
+  },
+};
+
 const yodobashi_url = 'http://www.yodobashi.com/%E4%BB%BB%E5%A4%A9%E5%A0%82-Nintendo-Nintendo-Switch-Joy-Con-L-%E3%83%8D%E3%82%AA%E3%83%B3%E3%83%96%E3%83%AB%E3%83%BC-R-%E3%83%8D%E3%82%AA%E3%83%B3%E3%83%AC%E3%83%83%E3%83%89-Nintendo-Switch%E6%9C%AC%E4%BD%93/pd/100000001003431566/';
 
 const getResult = (url) => {
@@ -31,23 +39,32 @@ const sendAttachments = (robot, room, attachments) => {
 module.exports = (robot) => {
   const cron_execute = /^production$/i.test(process.env.NODE_ENV);
   new CronJob('0 */1 * * * *', () => {
-    aa(function*() {
-      const $ = yield getResult(yodobashi_url);
-      if (!('error' in $)) {
-        const salesInfo = $('.buyBoxMain .salesInfo').text();
-        const redis_salesInfo = robot.brain.get('yodobashi_switch') || '';
+    const channel_id = robot.adapter.client.rtm.dataStore.getChannelOrGroupByName('watch_switch').name;
 
-        if (salesInfo !== redis_salesInfo) {
-          robot.brain.set('yodobashi_switch', salesInfo);
-          const channel_id = robot.adapter.client.rtm.dataStore.getChannelOrGroupByName('watch_switch').name;
+    Object.keys(watch_lists).forEach((key) => {
+      aa(function*() {
+        let message = null;
+        const $ = yield getResult(watch_lists[key].url);
+        if (!('error' in $)) {
+          const salesInfo = $(watch_lists[key].elm).text();
+          const redis_salesInfo = robot.brain.get(key + '_switch') || '';
+
+          if (salesInfo !== redis_salesInfo) {
+            message = watch_lists[key].name + ' [' + salesInfo + ']';
+          }
+        } else {
+          message = watch_lists[key].name + ' [Error]';
+        }
+
+        if (message) {
           const attachments = {
-            fallback: 'ヨドバシカメラ [' + salesInfo  + ']',
-            title: 'ヨドバシカメラ [' + salesInfo  + ']',
-            title_link: yodobashi_url,
+            fallback: message,
+            title: message,
+            title_link: watch_lists[key].url,
           };
           sendAttachments(robot, channel_id, attachments);
         }
-      }
+      });
     });
   }, null, cron_execute);
 };
